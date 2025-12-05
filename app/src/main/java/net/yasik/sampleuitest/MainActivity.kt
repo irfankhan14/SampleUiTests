d456f775a481b94678dc4929b0e75ac6f62cbdd3
irfankhan14 <irfankhan.ys@gmail.com>
2025-12-05 16:27:08 +0530

    Sample code concatenation with ui and logic

--- app/src/main/java/net/yasik/sampleuitest/MainActivity.kt
+++ app/src/main/java/net/yasik/sampleuitest/MainActivity.kt
@@ -3,18 +3,47 @@ package net.yasik.sampleuitest
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        result = findViewById(R.id.result)
+
+        val buttonOne = findViewById<Button>(R.id.button_one)
+        val buttonTwo = findViewById<Button>(R.id.button_two)
+        val buttonPlus = findViewById<Button>(R.id.button_plus)
+
+        buttonOne.setOnClickListener { addNumber(1) }
+        buttonTwo.setOnClickListener { addNumber(2) }
+        buttonPlus.setOnClickListener { addOperation() }
+    }
+
+    private fun addNumber(number: Int) {
+        val currentText = result.text.toString()
+        result.text = currentText + number
+    }
+
+    private fun addOperation() {
+        val currentText = result.text.toString()
+        result.text = "$currentText+"
+    }
+
+    fun calculate(expression: String): Int {
+        val parts = expression.split("\\+".toRegex()).toTypedArray()
+        var sum = 0
+        for (part in parts) {
+            sum += part.toInt()
         }
+        return sum
     }
 }
